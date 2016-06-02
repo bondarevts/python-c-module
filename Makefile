9d37384ed67d1ac7e2753e714c89901003c22f64
Timofei Bondarev <bondarevts@gmail.com>
2016-06-02 19:05:30 -0400

    Ported to work with Argument Clinic

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 all: spammodule.c
+	./clinic.py spammodule.c
 	python3 setup.py build_ext --inplace
 
 clean:
-	rm -rf *.so build
+	rm -rf *.so build clinic
--- spammodule.c
+++ spammodule.c
@@ -1,22 +1,33 @@
 #include <Python.h>
 
+/*[clinic input]
+module spam
+[clinic start generated code]*/
+/*[clinic end generated code: output=da39a3ee5e6b4b0d input=fb02dbff56054488]*/
+
+#include "clinic/spammodule.c.h"
+
+/*[clinic input]
+spam.system
+
+  statement: 's'
+    a statement string to run through the system call
+  /
+
+Execute a shell command.
+[clinic start generated code]*/
 
 static PyObject *
-spam_system(PyObject *self, PyObject *args)
+spam_system_impl(PyModuleDef *module, const char *statement)
+/*[clinic end generated code: output=abfb2f1f48e7f5a7 input=e4343d85b0fe35d6]*/
 {
-    const char *command;
-    int sts;
-
-    if (!PyArg_ParseTuple(args, "s", &command)) {
-        return NULL;
-    }
-    sts = system(command);
+    int sts = system(statement);
     return PyLong_FromLong(sts);
 }
 
 
 static PyMethodDef SpamMethods[] = {
-    {"system",  spam_system, METH_VARARGS, "Execute a shell command."},
+    SPAM_SYSTEM_METHODDEF
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
@@ -24,7 +35,7 @@ static PyMethodDef SpamMethods[] = {
 static struct PyModuleDef spammodule = {
    PyModuleDef_HEAD_INIT,
    "spam",                 /* name of module */
-   "module documentation", /* module documentation, may be NULL */
+   spam_system__doc__, /* module documentation, may be NULL */
    -1,                     /* size of per-interpreter state of the module,
                               or -1 if the module keeps state in global variables. */
    SpamMethods
