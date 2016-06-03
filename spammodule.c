7cf690ee64295d1b1833b165dca856bfa4004929
Timofei Bondarev <bondarevts@gmail.com>
2016-06-03 14:09:11 -0400

    fix offset

--- spammodule.c
+++ spammodule.c
@@ -32,12 +32,12 @@ static PyMethodDef SpamMethods[] = {
 
 
 static struct PyModuleDef spammodule = {
-   PyModuleDef_HEAD_INIT,
-   "spam",                 /* name of module */
-   spam_system__doc__, /* module documentation, may be NULL */
-   -1,                     /* size of per-interpreter state of the module,
-                              or -1 if the module keeps state in global variables. */
-   SpamMethods
+    PyModuleDef_HEAD_INIT,
+    "spam",                 /* name of module */
+    spam_system__doc__, /* module documentation, may be NULL */
+    -1,                     /* size of per-interpreter state of the module,
+                               or -1 if the module keeps state in global variables. */
+    SpamMethods
 };
 
 
